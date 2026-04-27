cb0e7e454efd7a54d3e72f4867e4c6e936fc92ab
Leandro Oliveira da Luz <leandrolvrz.dev@gmail.com>
2026-04-27 19:02:23 -0300

    Exercicio (Breve correção): Calcular minutos e horas

--- horas.c
+++ horas.c
@@ -2,14 +2,19 @@
 
 int main (){
 	float a, b, minutos, horas;
+
 	printf("Digite os segundos para saber os minutos:");
 	scanf("%f", &a);
+
 	minutos= a/60;
 	horas=minutos/60;
+
 	printf ("%.2f segundos é em minutos:%.2f min. E em horas %.2fh\n", a, minutos, horas);
-	printf("Para saber a quantidade em horas, digite os minutos:");
+	printf("Para saber a quantidade em horas, digite os minutos: ");
 	scanf("%f", &b);
+
 	horas=b/60;
-	printf("%.2f minutos é em horas:%.2fh", b, horas);
+	printf("%.2f minutos é em horas:%.2fh\n", b, horas);
+
 	return 0;
 }
